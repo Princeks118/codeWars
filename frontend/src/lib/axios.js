d87986221affb13fc96021e305f2388a2111c0b9
Princessk118 <princesk118@gmail.com>
2025-06-11 19:14:27 +0530

    done adding axios

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,5 +2,8 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL: `${import.meta.env.VITE_BACKEND_URL}/api/v1`,
-  withCredentials: true,
+    withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
