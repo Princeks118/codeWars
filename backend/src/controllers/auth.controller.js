0c389835c577e6da519c63104b259e58e1241fc8
Princessk118 <princesk118@gmail.com>
2025-06-11 19:24:52 +0530

    done adding sme sitein cookie

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -92,7 +92,7 @@ export const login = async (req , res)=>{
 
         res.cookie("jwt" , token , {
             httpOnly:true,
-            sameSite:"strict",
+            sameSite:"None",
             secure:process.env.NODE_ENV !== "development",
             maxAge:1000 * 60 * 60 * 24 * 7 // 7 days
         })
